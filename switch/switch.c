cadde3d88864b3372269a78127713ee8851dbd53
riyabasak52 <44066566+riyabasak52@users.noreply.github.com>
2019-10-20 00:22:24 +0530

    Update switch.c

--- switch/switch.c
+++ switch/switch.c
@@ -8,33 +8,32 @@ int main()
     int num,ch;
     printf("enter the  two digit number of your choice");
     scanf("%d",&num);
-    while(ch!=2){
-    printf("This is the following menu options\n");
-    printf("----------1.Choose here for incrementing the number-----------------\n");
-    printf("----------2.Choose here for decrementing the number-------------------\n");
-    printf("----------3.EXIT THE PROGRAM----------------------------\n");
-    printf("enter the choice");
-    scanf("%d",&ch);
+    while(ch!=3){
+        printf("This is the following menu options\n");
+        printf("----------1.Choose here for incrementing the number-----------------\n");
+        printf("----------2.Choose here for decrementing the number-------------------\n");
+        printf("----------3.EXIT THE PROGRAM----------------------------\n");
+        printf("enter the choice");
+        scanf("%d",&ch);
     
-    switch(num) {
-        case 1: {
-            int res;
-            res=incr(num);
-            printf("the incremented number is %d\n",res);
-            break;
+        switch(ch) {
+            case 1: {
+                int res;
+                res=incr(num);
+                printf("the incremented number is %d\n",res);
+                break;
+            }
+            case 2:{
+                int res;
+                res=decr(num);
+                printf("the decremented number is %d\n",res);
+                break;
+            }
+            case 3:{
+                exit(0);
+                break;
+            }
         }
-        case 2:{
-            int res;
-            res=decr(num);
-            printf("the decremented number is %d\n",res);
-            break;
-            
-        }
-        case 3:{
-            exit(0);
-            break;
-        }
-    }
     }       
     
 
@@ -48,7 +47,7 @@ int incr(int x)
 }
 int decr(int x)
 {
-    x=x-1;
+    x=x/2;
     return x;
 }
 
